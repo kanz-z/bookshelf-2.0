52e3aea6e12751c060123ce822ef8523fb2a5d34
kanz-z <131506375+kanz-z@users.noreply.github.com>
2025-02-28 06:31:14 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,25 +1,26 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+const Hapi = require('@hapi/hapi');
 
-const init = async () => {
-  const server = Hapi.server({
-    port: 9000,
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
-    routes: {
-      cors: {
-        origin: ["*"], // Izinkan semua domain
-        headers: ["Accept", "Content-Type", "Authorization"],
-        exposedHeaders: ["Authorization"],
-        additionalHeaders: ["X-Requested-With"],
-        credentials: true, // Jika butuh cookies/credentials
-      },
+const server = Hapi.server({
+  port: process.env.PORT || 9000, // Vercel bakal tentuin port otomatis
+  host: 'localhost',
+  routes: {
+    cors: {
+      origin: ['*'],
     },
+  },
+});
+
+const routes = require('./routes');
+server.route(routes);
+
+module.exports = async (req, res) => {
+  await server.initialize();
+  server.inject({
+    method: req.method,
+    url: req.url,
+    payload: req.body,
+    headers: req.headers,
+  }).then((response) => {
+    res.status(response.statusCode).send(response.result);
   });
-
-  server.route(routes);
-
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
-init();
