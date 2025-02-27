c93c05987cfed67b40568a487052a9f8d298f7db
kanz-z <131506375+kanz-z@users.noreply.github.com>
2025-02-28 06:51:38 +0700

    Update server.js try catch

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const Hapi = require('@hapi/hapi');
 
 const server = Hapi.server({
-  port: process.env.PORT || 0.0.0.0, // Vercel bakal tentuin port otomatis
-  host: 'localhost',
+  port: process.env.PORT || 9000,
+  host: process.env.HOST || '0.0.0.0',
   routes: {
     cors: {
       origin: ['*'],
@@ -14,13 +14,17 @@ const routes = require('./routes');
 server.route(routes);
 
 module.exports = async (req, res) => {
-  await server.initialize();
-  server.inject({
-    method: req.method,
-    url: req.url,
-    payload: req.body,
-    headers: req.headers,
-  }).then((response) => {
+  try {
+    await server.initialize();
+    const response = await server.inject({
+      method: req.method,
+      url: req.url,
+      payload: req.body,
+      headers: req.headers,
+    });
     res.status(response.statusCode).send(response.result);
-  });
+  } catch (error) {
+    console.error('Error occurred:', error); // Log error to console
+    res.status(500).send({ error: 'Internal Server Error', message: error.message });
+  }
 };
